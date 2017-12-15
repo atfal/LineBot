e783803bd717151488d930ed4d23b59b0e913a6d
atfal <34569079+atfal@users.noreply.github.com>
2017-12-15 16:37:02 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -88,7 +88,7 @@ class Talk:
   def qrLogin(self, callback):
     self.transport.path = self.auth_query_path
 
-    qr = self.client.getAuthQrcode(True, "Ard Bot")
+    qr = self.client.getAuthQrcode(True, "AtfalR")
     callback("Copy to Line and Click\nYour LINK QR is: line://au/q/" + qr.verifier)
 
     r = requests.get("https://" + self.host + self.wait_for_mobile_path, headers={
