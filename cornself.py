696e0eb27c73e6f0b515e5562b744ce04f1d0f4c
atfal <34569079+atfal@users.noreply.github.com>
2017-12-15 16:30:31 +0700

    Add files via upload

---
+++ cornself.py
@@ -0,0 +1,503 @@
+# -*- coding: utf-8 -*-
+
+import LINETCR
+from LINETCR.lib.curve.ttypes import *
+from datetime import datetime
+import time,random,sys,json,codecs,threading,glob,re
+
+cl = LINETCR.LINE()
+cl.login(token="EnLesTtasmVE3X1KL3la.nSZCiUA6AEHxa7+nnQkRYG.cZGDpyxME3xFYJDbtCUs0I8WajHGo0YojTW06uWYlrc=")
+#cl.login(token="")
+cl.loginResult()
+
+cl
+
+print "login success"
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+helpmsg ="""
+================================
+                    CORN SELFBOT
+================================
+
+➣Mid @ - Show MID by tag
+➣Check: [MID] - Show contact by MID
+➣Cname:[name] - Change Name
+➣Nk @ - kick by tag
+➣Gift @ - Gift by tag
+➣copy @ - copy by tag
+➣speed - selfbot speed
+➣backup - reset from copy @
+➣Glist - Group joined list
+➣Gbc - Group Broadcast
+➣Cbc - Contact Broadcast
+➣Ourl - Open URL group
+➣Curl - Close URL Group
+➣group pict - Steal group picture
+➣Steal dp @ - Steal display picture
+➣Steal home @ - Steal home photo
+➣Invite [mid] - invite by mid
+➣Gname [name] - change group name
+➣Gift - send gift
+➣Me - send contact myself
+➣Tagall - Mention all members
+
+=================================
+http://line.me/ti/p/~atfaaal
+=================================
+"""
+KAC=[cl]
+mid = cl.getProfile().mid
+
+Bots=[mid]
+admin=["u36ccdc22e6ba69e85703bfcd36a2abca"]
+wait = {
+    'contact':True,
+    'autoJoin':True,
+    'autoCancel':{"on":True,"members":1},
+    'leaveRoom':True,
+    'timeline':True,
+    'autoAdd':True,
+    "lang":"JP",
+    "commentOn":False,
+    "commentBlack":{},
+    "wblack":False,
+    "dblack":False,
+    "clock":False,
+    "blacklist":{},
+    "wblacklist":False,
+    "dblacklist":False,
+    "protectionOn":True,
+    "atjointicket":False
+    }
+
+wait2 = {
+    'readPoint':{},
+    'readMember':{},
+    'setTime':{},
+    'ROM':{}
+    }
+
+mimic = {
+    "copy":False,
+    "copy2":False,
+    "status":False,
+    "target":{}
+    }
+
+setTime = {}
+setTime = wait2['setTime']
+
+contact = cl.getProfile()
+backup = cl.getProfile()
+backup.displayName = contact.displayName
+backup.statusMessage = contact.statusMessage                        
+backup.pictureStatus = contact.pictureStatus
+
+def sendMessage(to, text, contentMetadata={}, contentType=0):
+    mes = Message()
+    mes.to, mes.from_ = to, profile.mid
+    mes.text = text
+    mes.contentType, mes.contentMetadata = contentType, contentMetadata
+    if to not in messageReq:
+        messageReq[to] = -1
+    messageReq[to] += 1
+
+
+def NOTIFIED_READ_MESSAGE(op):
+    try:
+        if op.param1 in wait2['readPoint']:
+            Name = cl.getContact(op.param2).displayName
+            if Name in wait2['readMember'][op.param1]:
+                pass
+            else:
+                wait2['readMember'][op.param1] += "\n・" + Name
+                wait2['ROM'][op.param1][op.param2] = "・" + Name
+        else:
+            pass
+    except:
+      pass
+
+
+def bot(op):
+    try:
+        if op.type == 0:
+            return
+        if op.type == 13:
+                if op.param3 in mid:
+                    if op.param2 in Amid:
+                        G = ki.getGroup(op.param1)
+                        G.preventJoinByTicket = False
+                        ki.updateGroup(G)
+                        Ticket = ki.reissueGroupTicket(op.param1)
+                        cl.acceptGroupInvitationByTicket(op.param1,Ticket)
+                        G.preventJoinByTicket = True
+                        ki.updateGroup(G)
+                        Ticket = ki.reissueGroupTicket(op.param1)
+        if op.type == 26:
+            msg = op.message
+            if "@"+cl.getProfile().displayName in msg.text:
+                 xname = cl.getContact(msg.from_).displayName
+                 xlen = str(len(xname)+1)
+                 kata = "apaan "
+                 msg.contentType = 0
+                 msg.text = kata+"@"+xname
+                 msg.contentMetadata = {'MENTION':{"MENTIONEES": [{"S":"0","E":'+json.dumps(xlen)+',"M":'+json.dumps(msg.from_)+'}]},'EMTVER':'4'}
+                 try:
+                     cl.sendMessage(msg)
+                 except Exception as e:
+                     print e
+
+        if op.type == 25:
+            msg = op.message
+            if msg.text in ["Speed","speed","sp"]:
+                    start = time.time()
+                    elapsed_time = time.time() - start
+                    cl.sendText(msg.to, "%sseconds" % (elapsed_time))  
+            elif msg.text in ["me","Me"]:
+                msg.contentType = 13
+                msg.contentMetadata = {'mid': mid}
+                cl.sendMessage(msg)
+            elif "Steal dp @" in msg.text:
+                print "[Command]dp executing"
+                _name = msg.text.replace("Steal dp @","")
+                _nametarget = _name.rstrip('  ')
+                gs = cl.getGroup(msg.to)
+                targets = []
+                for g in gs.members:
+                    if _nametarget == g.displayName:
+                        targets.append(g.mid)
+                if targets == []:
+                    ki.sendText(msg.to,"Contact not found")
+                else:
+                    for target in targets:
+                        try:
+                            contact = cl.getContact(target)
+                            path = "http://dl.profile.line-cdn.net/" + contact.pictureStatus
+                            cl.sendImageWithURL(msg.to, path)
+                        except:
+                            pass
+                print "[Command]dp executed"
+            elif "rejectall" in msg.text:
+                    X = cl.getGroupIdsInvited()
+                    for i in X:
+                       cl.rejectGroupInvitation(i)
+            elif "Steal home @" in msg.text:
+                print "[Command]dp executing"
+                _name = msg.text.replace("Steal home @","")    
+                _nametarget = _name.rstrip('  ')
+                gs = cl.getGroup(msg.to)
+                targets = []
+                for g in gs.members:
+                    if _nametarget == g.displayName:
+                        targets.append(g.mid)
+                if targets == []:
+                    ki.sendText(msg.to,"Contact not found")
+                else:
+                    for target in targets:
+                        try:
+                            contact = cl.getContact(target)
+                            cu = cl.channel.getCover(target)          
+                            path = str(cu)
+                            cl.sendImageWithURL(msg.to, path)
+                        except:
+                            pass
+                print "[Command]dp executed"
+            elif msg.text in ["Curl","curl"]:
+                if msg.toType == 2:                          
+                    X = cl.getGroup(msg.to)
+                    X.preventJoinByTicket = True
+                    cl.updateGroup(X)
+                    if wait["lang"] == "JP":
+                        cl.sendText(msg.to,"Done")
+                    else:
+                        cl.sendText(msg.to,"already close")
+            elif msg.text in ["ourl","Ourl"]:
+              if msg.toType == 2:
+                    X = cl.getGroup(msg.to)
+                    X.preventJoinByTicket = False
+                    cl.updateGroup(X)
+                    if wait["lang"] == "JP":
+                        cl.sendText(msg.to,"Done")
+                    else:
+                        cl.sendText(msg.to,"already open")
+            elif "gift" in msg.text:
+                msg.contentType = 9
+                msg.contentMetadata={'PRDID': 'a0768339-c2d3-4189-9653-2909e9bb6f58',
+                                    'PRDTYPE': 'THEME',
+                                    'MSGTPL': '5'}
+                msg.text = None
+                cl.sendMessage(msg)
+            elif "mid @" in msg.text.lower():
+                _name = msg.text.lower().replace("mid @","")
+                _nametarget = _name.rstrip(' ')
+                gs = cl.getGroup(msg.to)
+                for g in gs.members:
+                    if _nametarget == g.displayName:
+                        cl.sendText(msg.to, g.mid)
+                    else:
+                        pass
+            elif msg.text in ["tagall","Tagall"]:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
+
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
+            #------------Show contact dari mid-----------#
+            elif "Invite " in msg.text:
+                    midd = msg.text.replace("Invite ","")
+                    cl.findAndAddContactsByMid(midd)
+                    cl.inviteIntoGroup(msg.to,[midd])
+            elif ("Gname " in msg.text):
+                if msg.toType == 2:
+                    X = cl.getGroup(msg.to)
+                    X.name = msg.text.replace("Gname ","")
+                    cl.updateGroup(X)
+            elif "Check:" in msg.text:
+                    midd = msg.text.replace("Check:","")
+                    msg.contentType = 13
+                    msg.contentMetadata = {"mid":midd}
+                    cl.sendMessage(msg)
+            elif "Cname:" in msg.text:
+                string = msg.text.replace("Cname:","")
+                if len(string.decode('utf-8')) <= 10000000000:
+                    profile = cl.getProfile()
+                    profile.displayName = string
+                    cl.updateProfile(profile)
+                    cl.sendText(msg.to,"Changed " + string + "")
+            elif "nk " in msg.text:  
+                   key = eval(msg.contentMetadata["MENTION"])
+                   key["MENTIONEES"][0]["M"]  
+                   targets = []
+                   for x in key["MENTIONEES"]:
+                        targets.append(x["M"])            
+                   for target in targets:
+                        try:                                       
+                            cl.kickoutFromGroup(msg.to,[target])         
+                        except:                                   
+                            pass
+            elif ("Mkick " in msg.text):
+                   targets = []
+                   key = eval(msg.contentMetadata["MENTION"])
+                   key["MENTIONEES"] [0] ["M"]
+                   for x in key["MENTIONEES"]:
+                       targets.append(x["M"])
+                   for target in targets:
+                       try:
+                           random.choice(KAC).kickoutFromGroup(msg.to,[target])
+                       except:
+                           cl.sendText(msg.to,"Error")
+            elif "Gbc " in msg.text:
+                    bctxt = msg.text.replace("Gbc ", "")
+                    n = cl.getGroupIdsJoined()
+                    for manusia in n:
+                        cl.sendText(manusia, (bctxt))
+
+            elif "Cbc " in msg.text:
+                    bctxt = msg.text.replace("Contact bc ", "")
+                    t = cl.getAllContactIds()
+                    for manusia in t:
+                        cl.sendText(manusia, (bctxt))
+            elif "Gift @" in msg.text:
+                _name = msg.text.replace("Gift @","")
+                _nametarget = _name.rstrip(' ')
+                gs = cl.getGroup(msg.to)
+                for g in gs.members:
+                    if _nametarget == g.displayName:
+                        msg.contentType = 9
+                        msg.contentMetadata={'PRDID': '89131c1a-e549-4bd5-9e60-e24de0d2e252',
+                                    'PRDTYPE': 'THEME',
+                                    'MSGTPL': '10'}
+                        msg.text = None
+                        cl.sendMessage(msg,g)
+            elif "copy @" in msg.text:
+                   print "[COPY] Ok"
+                   _name = msg.text.replace("copy @","")
+                   _nametarget = _name.rstrip('  ')
+                   gs = cl.getGroup(msg.to)
+                   targets = []
+                   for g in gs.members:
+                       if _nametarget == g.displayName:
+                           targets.append(g.mid)
+                   if targets == []:
+                       cl.sendText(msg.to, "Not Found...")
+                   else:
+                       for target in targets:
+                            try:
+                               cl.CloneContactProfile(target)
+                               cl.sendText(msg.to, "Copied.")
+                            except Exception as e:
+                                print e
+            elif msg.text.lower() =="group pict":
+                ab = cl.getGroup(msg.to).pictureStatus
+                cl.sendImageWithURL(msg.to,"http://dl.profile.line-cdn.net/" + ab)
+            elif "Steal: " in msg.text:
+                    salsa = msg.text.replace("Steal: ","")
+                    Manis = cl.getContact(salsa)
+                    Imoet = "http://dl.profile.line-cdn.net/" + contact.pictureStatus
+                    try:
+                        cover = cl.channel.getCover(Manis)
+                    except:
+                        cover = ""
+                    cl.sendText(msg.to,"Gambar Foto Profilenya")
+                    cl.sendImageWithURL(msg.to,Imoet)
+                    if cover == "":
+                        cl.sendText(msg.to,"User tidak memiliki cover atau sejenisnya")
+                    else:
+                        cl.sendText(msg.to,"Gambar Covernya")
+                        cl.sendImageWithURL(msg.to,cover)
+            elif msg.text in ["Backup","backup"]:
+                try:
+                    cl.updateDisplayPicture(backup.pictureStatus)
+                    cl.updateProfile(backup)
+                    cl.sendText(msg.to, "Refreshed.")
+                except Exception as e:
+                    cl.sendText(msg.to, str(e))
+            elif msg.text in ["help","key"]:
+                cl.sendText(msg.to,helpmsg)
+            elif msg.text in ["List group","Glist"]:
+							gid = cl.getGroupIdsJoined()
+							h = ""
+							for i in gid:
+								h += "[⭐] %s  \n" % (cl.getGroup(i).name + " | Members : " + str(len (cl.getGroup(i).members)))
+								cl.sendText(msg.to, "☆「Group List」☆\n"+ h +"Total Group : " +str(len(gid)))
+            elif msg.from_ in mimic["target"] and mimic["status"] == True and mimic["target"][msg.from_] == True:
+            	text = msg.text
+            	if text is not None:
+            		cl.sendText(msg.to,text)
+            	else:
+            		if msg.contentType == 7:
+            			msg.contentType = 7
+            			msg.text = None
+            			msg.contentMetadata = {
+            			"STKID": "6",
+            			"STKPKGID": "1",            						"STKVER": "100" }
+            			cl.sendMessage(msg)
+            elif msg.contentType == 13:
+            			msg.contentType = 13
+            			msg.contentMetadata = {'mid': msg.contentMetadata["mid"]}
+            			cl.sendMessage(msg)
+            elif "Mimic:" in msg.text:
+            		cmd = msg.text.replace("Mimic:","")
+            		if cmd == "on":
+            			if mimic["status"] == False:
+            				mimic["status"] = True
+            				cl.sendText(msg.to,"Mimic on")
+            			else:
+            				cl.sendText(msg.to,"Mimic already on")
+            		elif cmd == "off":
+            			if mimic["status"] == True:
+            				mimic["status"] = False
+            				cl.sendText(msg.to,"Mimic off")
+            			else:
+            				cl.sendText(msg.to,"Mimic already off")
+            		elif "add:" in cmd:
+            			target0 = msg.text.replace("Mimic:add:","")
+            			target1 = target0.lstrip()
+            			target2 = target1.replace("@","")
+            			target3 = target2.rstrip()
+            			_name = target3
+            			gInfo = cl.getGroup(msg.to)
+            			targets = []
+            			for a in gInfo.members:
+            				if _name == a.displayName:
+            					targets.append(a.mid)
+            			if targets == []:
+            				cl.sendText(msg.to,"No targets")
+            			else:
+            				for target in targets:
+            					try:
+            						mimic["target"][target] = True
+            						cl.sendText(msg.to,"Success added target")
+            						#cl.sendMessageWithMention(msg.to,target)
+            						break
+            					except:
+            						cl.sendText(msg.to,"Failed")
+            						break
+            		elif "del:" in cmd:
+            			target0 = msg.text.replace("Mimic:del:","")
+            			target1 = target0.lstrip()
+            			target2 = target1.replace("@","")
+            			target3 = target2.rstrip()
+            			_name = target3
+            			gInfo = cl.getGroup(msg.to)
+            			targets = []
+            			for a in gInfo.members:
+            				if _name == a.displayName:
+            					targets.append(a.mid)
+            			if targets == []:
+            				cl.sendText(msg.to,"No targets")
+            			else:
+            				for target in targets:
+            					try:
+            						del mimic["target"][target]
+            						cl.sendText(msg.to,"Success deleted target")
+            						#cl.sendMessageWithMention(msg.to,target)
+            						break
+            					except:
+            						cl.sendText(msg.to,"Failed!")
+            						break
+            		elif cmd == "ListTarget":
+            			if mimic["target"] == {}:
+            				cl.sendText(msg.to,"No target")
+                    	else:
+                    		lst = "<<Lit Target>>"
+                    		total = len(mimic["target"])
+                    		for a in mimic["target"]:
+                				if mimic["target"][a] == True:
+                					stat = "On"
+                				else:
+                					stat = "Off"
+                				lst += "\n->" + cl.getContact(mi_d).displayName + " | " + stat
+                                cl.sendText(msg.to,lst + "\nTotal:" + total)
+
+
+        if op.type == 59:
+            print op
+
+
+    except Exception as error:
+        print error
+
+
+def a2():
+    now2 = datetime.now()
+    nowT = datetime.strftime(now2,"%M")
+    if nowT[14:] in ["10","20","30","40","50","00"]:
+        return False
+    else:
+        return True
+
+while True:
+    try:
+        Ops = cl.fetchOps(cl.Poll.rev, 5)
+    except EOFError:
+        raise Exception("It might be wrong revision\n" + str(cl.Poll.rev))
+
+    for Op in Ops:
+        if (Op.type != OpType.END_OF_OPERATION):
+            cl.Poll.rev = max(cl.Poll.rev, Op.revision)
+            bot(Op)
+            
+
